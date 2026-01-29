140a7db3a7aa0a21e5bd4f723c4279a065076cdf
devinjohnson <devin.johnson-11@selu.edu>
2026-01-29 11:28:30 -0600

    assignment save

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter age: ");
+        int age = input.nextInt();
+        boolean isValidAge;
+        if (age >= 18 && age <=65) {
+            isValidAge = true;
+        } else {
+            isValidAge = false;
+        }
+
+
+        System.out.println("Enter Security Code: ");
+        int code = input.nextInt();
+        boolean isValidCode;
+        if (code % 10 == 0){
+            isValidCode = false;
+        }else if (code % 5 == 0 || code % 7 == 0){
+            isValidCode = true;
+        }
+
+
+        System.out.println("Enter Clearance Level: ");
+        int clearance = input.nextInt();
+        boolean isBlocked;
+        if (clearance <= 3 && clearance >= 1){
+            isBlocked = false;
+        } else
+            isBlocked = false;
+
+
+        /*
+                 isValidAge: True if Age is between 18 and 65 (inclusive).
+         isValidCode: True if the Security Code meets all of the following criteria:
+         It is divisible by 5 OR divisible by 7.
+         AND... it is NOT divisible by 10.
+         isBlocked: True if the Clearance Level is NOT 1, 2, or 3.
+        */
+
+
+
+
     }
 }
